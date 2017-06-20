1500ba9379fc754c600433efb8410b6ff6cc39a1
Innos <victor.d.kazakov@gmail.com>
2017-06-21 01:27:39 +0300

    Added ModularArithmetic - ModularMultiplicativeInverse

---
+++ ModularArithmetic/01ModularMultiplicativeInverse/ModularMultiplicativeInverse.cs
@@ -0,0 +1,40 @@
+﻿namespace _01ModularMultiplicativeInverse
+{
+    using System;
+
+    public class ModularMultiplicativeInverse
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(FindModularMultiplicativeInverse(5, 7));
+            Console.WriteLine(FindModularMultiplicativeInverse(4, 9));
+            Console.WriteLine(FindModularMultiplicativeInverse(7, 5));
+            Console.WriteLine(FindModularMultiplicativeInverse(9, 4));
+        }
+
+        // Solves at = 1 (mod n) - congruence 
+        private static int FindModularMultiplicativeInverse(int a, int n)
+        {
+            var t = 0;
+            var newT = 1;
+            var r = n;
+            var newR = a;
+
+            while (newR != 0)
+            {
+                var q = r / newR;
+                var oldT = t;
+                t = newT;
+                newT = oldT - q * newT;
+
+                var oldR = r;
+                r = newR;
+                newR = oldR - q * newR;
+            }
+
+            if (r > 1) return -1;
+            if (t < 0) t = t + n;
+            return t;
+        }
+    }
+}
